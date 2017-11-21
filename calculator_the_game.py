4089019b814ff9eff6880b9cf0d0ddbe98b2c4e6
ylkangpeter <ylkangpeter@gmail.com>
2017-11-21 09:08:43 -0600

    Update calculator_the_game.py
    more features

--- calculator_the_game.py
+++ calculator_the_game.py
@@ -1,5 +1,5 @@
-#!/bin/sh
 # -*- coding: utf-8 -*-
+
 import itertools
 from decimal import Decimal
 
@@ -18,7 +18,7 @@ def gen(condition, steps):
         tmp = list(allSteps[i])
         for j in xrange(0, len(condition)):
             tmp.append(condition[j])
-        re.append(tmp);
+        re.append(tmp)
     tmpRe = set()
     rr = []
     for i in xrange(0, len(re)):
@@ -31,60 +31,84 @@ def gen(condition, steps):
 
     allSteps = []
     for i in xrange(0, len(rr)):
-        allSteps.extend(list(itertools.permutations(rr[i], steps)));
+        allSteps.extend(list(itertools.permutations(rr[i], steps)))
     return allSteps
 
 
 def doBinaryMath(initVal, operation):
-    sign = operation[0]
-    t = 0
-
-    if sign == "-":
-        t = initVal + Decimal(operation)
-    elif sign == "+":
-        if operation == "+":
-            t = initVal * (-1)
-        else:
-            t = initVal + Decimal(operation)
-    elif sign == "*":
-        t = initVal * Decimal(operation[1:])
-    elif sign == "/":
-        t = initVal / Decimal(operation[1:])
-    elif sign == "r":
-        t = Decimal(str(initVal)[::-1])
-    elif sign == "<":
-        t = int(initVal / 10)
+    if operation.__contains__("=>"):
+        vals = operation.split("=>")
+        tmp = str(initVal)
+        return Decimal(tmp.replace(vals[0], vals[1]))
     else:
-        if initVal >= 0:
-            t = initVal * 10 + Decimal(operation)
-        else:
-            t = initVal * 10 - Decimal(operation)
+        sign = operation[0]
+        t = 0
 
+        if sign == "-":
+            t = initVal + Decimal(operation)
+        elif sign == "m":
+            tmp = str(initVal)
+            t = Decimal(tmp + tmp[::-1])
+        elif sign == "+":
+            if operation == "+":
+                t = initVal * (-1)
+            else:
+                t = initVal + Decimal(operation)
+        elif sign == "*":
+            t = initVal * Decimal(operation[1:])
+        elif sign == "/":
+            t = initVal / Decimal(operation[1:])
+        elif sign == "r":
+            t = Decimal(str(initVal)[::-1])
+        elif sign == "<":
+            t = int(initVal / 10)
+        else:
+            if initVal >= 0:
+                t = initVal * 10 + Decimal(operation)
+            else:
+                t = initVal * 10 - Decimal(operation)
     return t
 
 
-def test(comb, initVal):
-    for j in xrange(0, len(comb)):
-        initVal = doBinaryMath(initVal, comb[j])
-    return initVal
+# def test(comb, initVal):
+#     for j in xrange(0, len(comb)):
+#         initVal = doBinaryMath(initVal, comb[j])
+#     return initVal
 
 
 def calc(combinations, result, initVal):
     for i in xrange(0, len(combinations)):
         val = Decimal(initVal)
-        for j in xrange(0, len(combinations[i])):
-            val = doBinaryMath(val, combinations[i][j])
+        tmps = list(combinations[i])
+
+        for j in xrange(0, len(tmps)):
+            if tmps[j].startswith("["):
+                v = tmps[j].split(" ")
+                for m in xrange(0, len(tmps)):
+                    tmps[m] = decorate(tmps[m], v[0][1], v[1]);
+                else:
+                    val = doBinaryMath(val, tmps[j])
         # print "%s %d" % (combinations[i], val)
         if val == result:
             print "result: %s" % str(combinations[i])
 
 
-test(['-9', '-9', '<', '+', '-9', '*3'], 111)
+def decorate(raw, oper, addup):
+    if raw == "-" or raw == "m" or raw == "<" or raw == "r" or raw == "+" or raw.__contains__("=>") or raw[0] == "[":
+        return;
+    elif raw.isdigit():
+        return eval(raw + oper + addup)
+    else:
+        return raw[0] + str(eval(raw[1:] + oper + addup))
 
-initVal = 111
-condition = ["*3", "-9", "+", "<"]
-steps = 6
-result = 126
+
+        # test(['+2', '1', '[+ 3'], 0)
+
+
+initVal = 0
+condition = ['+2', '1', '[+ 3']
+steps = 5
+result = 28
 
 combinations = gen(condition, steps)
 # print combinations
